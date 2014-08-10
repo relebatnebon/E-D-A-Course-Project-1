85084c65196fe9658e5cac2d5cae543908b64da0
relebatnebon <bentabeler@gmail.com>
2014-08-10 23:27:05 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,47 +1,29 @@
-## This program absorbs the dataset and produce a graph.
-## The code does the following:
-## 	asks for the location of the dataset, 
-## 	reads the dataset,
-## 	subsets the data,
-## 	modify the time column, 
-## 	plots the histogram, 
-## 	and save the output to a PNG graphic file in the working directory.
+## Input: Dataset position
+## Output: Chart showing the level of 3 energy sub meterings
 
 ## ask for location of the source file
-print("Please enter the position of the data file under the working directory.")
-print("If your dataset is placed in the folder named Data under the working directory")
-print(" and named as household_power_consumption.txt,")
-print(" the input should be ./Data/household_power_consumption.txt.")
-cat("Position of the dataset:")
+## Input example: ./Data/household_power_consumption.txt
+cat("Source file location:")
 dirSource <- readLines(n=1)
 
 ## set data types
 library(methods)
-setClass("myDate")
-setClass("myTime")
-setAs("character", "myDate", function(from) as.Date(from, format="%d/%m/%Y") )
-setAs("character", "myTime", function(from) as.POSIXct(from, format="%H:%M:%S"))
+setClass("thisDate")
+setAs("character", "thisDate", function(from) as.Date(from, format="%d/%m/%Y") )
+setClass("thisTime")
+setAs("character", "thisTime", function(from) as.POSIXct(from, format="%H:%M:%S"))
 
-## read data with data type specifications
-rawDataset <- read.table(dirSource,
-	header=TRUE,
-	na.strings="?",
-	colClasses=c("myDate","myTime",rep("numeric",7)),
-	sep=";")
+## read data
+rawDataset <- read.table(dirSource,header=TRUE,na.strings="?",
+		colClasses=c("thisDate","thisTime",rep("numeric",7)),sep=";")
 
 ## subset the data
-ds <- rawDataset[
-	rawDataset$Date >= as.Date("2007-02-01") &
-	rawDataset$Date <= as.Date("2007-02-02"),]
+ds <- rawDataset[rawDataset$Date >= as.Date("2007-02-01") &
+			rawDataset$Date <= as.Date("2007-02-02"),]
 
-## modify time column
-ds$Time <-ISOdatetime(
-	format(ds$Date, "%Y")
-	,format(ds$Date, "%m")
-	,format(ds$Date, "%d") 
-	,format(ds$Time, "%H") 
-	,format(ds$Time, "%M") 
-	,format(ds$Time, "%S")
+## update Time column
+ds$Time <-ISOdatetime(format(ds$Date, "%Y"),format(ds$Date, "%m"),format(ds$Date, "%d") 
+	,format(ds$Time, "%H"),format(ds$Time, "%M"),format(ds$Time, "%S")
 	,tz = "")
 
 ## create plot 3
